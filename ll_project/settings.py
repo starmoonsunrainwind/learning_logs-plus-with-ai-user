791e8071fdd587120e3ef1d30d25ac77def00764
陈泽菁 <3356419874@qq.com>
2026-08-15 19:08:49 +0800

    添加配置

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <--- 加上这一行！
 """我的设置"""
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
